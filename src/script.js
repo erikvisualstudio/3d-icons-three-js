e77e8719d29464a741f03f72c8961af969062da5
erikvisualstudio <neuhaus.erik@gmx.de>
2025-11-21 09:35:07 +0100

    figma-icon-build-2

--- src/script.js
+++ src/script.js
@@ -27,7 +27,7 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   100
 );
-camera.position.set(8, 0, 16);  // von links & leicht oben
+camera.position.set(9.5, 0, 19);  // von links & leicht oben
 camera.lookAt(0, 0, 0);
 
 scene.add(camera);
@@ -36,7 +36,7 @@ scene.add(camera);
 const textureLoader = new THREE.TextureLoader();
 
 // selbe Datei als einfache "Fake"-EnvMap benutzen
-const envTexture = textureLoader.load('textures/yellow-light-background.jpg');
+const envTexture = textureLoader.load('textures/blue-light-background.jpg');
 envTexture.mapping = THREE.EquirectangularReflectionMapping;
 envTexture.colorSpace = THREE.SRGBColorSpace;
 
