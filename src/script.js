912f6c9c062925e0a6113375e0ca92ffd6b82543
erikvisualstudio <neuhaus.erik@gmx.de>
2025-11-21 09:21:49 +0100

    arrow-icon-build-1

--- src/script.js
+++ src/script.js
@@ -27,7 +27,7 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   100
 );
-camera.position.set(5.85, 0, 11.7);  // von links & leicht oben
+camera.position.set(8.5, 0, 17);  // von links & leicht oben
 camera.lookAt(0, 0, 0);
 
 scene.add(camera);
@@ -36,7 +36,7 @@ scene.add(camera);
 const textureLoader = new THREE.TextureLoader();
 
 // selbe Datei als einfache "Fake"-EnvMap benutzen
-const envTexture = textureLoader.load('textures/blue-orange-background.jpg');
+const envTexture = textureLoader.load('textures/green-red-background.jpg');
 envTexture.mapping = THREE.EquirectangularReflectionMapping;
 envTexture.colorSpace = THREE.SRGBColorSpace;
 
@@ -63,7 +63,7 @@ const loader = new GLTFLoader();
 let model = null;
 
 loader.load(
-  '3D/Branding.glb',
+  '3D/Pfeil.glb',
   (gltf) => {
     model = gltf.scene;
 
@@ -91,6 +91,7 @@ model.traverse((child) => {
 
 // Model selbst wieder an (0,0,0) setzen
 model.position.set(0, 0, 0);
+model.rotation.z = THREE.MathUtils.degToRad(-45);
 
     scene.add(model);
 
